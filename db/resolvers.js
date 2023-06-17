13a04076932d75b1ccc2687cc52a1c7886c87c1d
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2023-06-17 16:39:42 +0200

    creating users model and connecting to database

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,10 +1,23 @@
 //Resolvers
-
+const User = require('../models/User');
 
 const resolvers = {
     Query: {
         obtenerCursos: () => "Algo"
 
+    },
+    Mutation: {
+        newUser: async (_, {input}) => {
+
+            const {email, password} = input
+            //Check if user is registered
+            const userExists = await User.findOne({email});
+            console.log(userExists);
+            
+            //Hash password
+
+            // Save to db
+        }
     }
 }
 
--- db/schema.js
+++ db/schema.js
@@ -3,9 +3,31 @@ const { gql } = require('apollo-server')
 //Schema
 
 const typeDefs = gql`
+
+    type User {
+        id: ID
+        nombre: String
+        apellido: String
+        email: String
+        creado: String
+    }
+
+    input UserInput {
+        nombre: String!
+        apellido: String!
+        email: String!
+        password: String!
+    }
+
     type Query {
         obtenerCursos: String
     }
+
+    type Mutation {
+        newUser(input: UserInput): String
+    }
+
+
 `;
 
 module.exports = typeDefs
