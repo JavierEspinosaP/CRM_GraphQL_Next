eb5beb49bfc1cba8fce6cc769563902f0a8f2394
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2023-07-05 13:16:41 +0200

    add/head_tag_and_sidebar_test

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,7 +1,7 @@
-import './globals.css'
 import { Inter } from 'next/font/google'
-
 const inter = Inter({ subsets: ['latin'] })
+
+import Sidebar from './components/Sidebar'
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,8 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <title>CRM CLIENT ADMINISTRATION</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <script src="https://cdn.tailwindcss.com"></script>
+      </head>
       <body className={inter.className}>
         
+        <Sidebar/>
         {children}
 
         </body>
