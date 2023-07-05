a76403318a1bcd7c65c2c15d1ddbc91374c5a5ec
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2023-07-05 12:59:16 +0200

    add/layout_config_and_page_test

---
+++ client/app/prueba/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../layout'
+
+function Prueba() {
+  return (
+    <Layout>
+    <h1>Desde prueba</h1>        
+    </Layout>
+
+  )
+}
+
+export default Prueba
