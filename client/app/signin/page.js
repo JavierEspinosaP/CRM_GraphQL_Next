0af173cfba06ab819da884b061fee23ddd6e7f28
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2023-07-19 22:58:27 +0200

    add/formik_yup

--- client/app/signin/page.js
+++ client/app/signin/page.js
@@ -1,19 +1,43 @@
+'use client'
+
 import React from 'react'
+import {useFormik} from 'formik'
+import * as Yup from 'yup'
 
 function SignIn() {
+
+
+    //Form validation
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            surname: '',
+            email:'',
+            password:''
+        },
+        onSubmit: values => {
+            console.log(values);
+        }
+    })
+
   return (
     <section className='min-h-screen flex flex-col justify-center w-screen'>
     <h1 className='text-center'>Sign In</h1>
     <div className='flex justify-center'>
         <div className=' w-full max-w-sm'>
-            <form className='bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4'>
+            <form   className='bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4'
+                    onSubmit={formik.handleSubmit}>
             <div className='mb-4'>
                     <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='name'>Nombre</label>
                     <input
                         className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2'
                         id='name'
                         type='text'
-                        placeholder='John' />
+                        placeholder='John' 
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        />
                 </div>
                 <div className='mb-4'>
                     <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='surname'>Apellido</label>
@@ -21,7 +45,10 @@ function SignIn() {
                         className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2'
                         id='surname'
                         type='text'
-                        placeholder='Doe' />
+                        placeholder='Doe'
+                        value={formik.values.surname}
+                        onChange={formik.handleChange}
+                         />
                 </div>
                 <div className='mb-4'>
                     <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='email'>Email</label>
@@ -29,7 +56,10 @@ function SignIn() {
                         className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2'
                         id='email'
                         type='email'
-                        placeholder='abc@mail.com' />
+                        placeholder='abc@mail.com'
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        />
                 </div>
                 <div className='mb-4'>
                     <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='password'>Password</label>
@@ -37,7 +67,10 @@ function SignIn() {
                         className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2'
                         id='password'
                         type='password'
-                        placeholder='1234abcde' />
+                        placeholder='1234abcde'
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                         />
                 </div>
                 <input
                     type='submit'
